737df22151d231199430aaab12e61a2954e733fe
hayoung Park <mcd158@naver.com>
2025-04-09 18:19:43 +0900

    fix : 불필요한 파일 정리 및 경로 수정

--- src/pages/WriteReviewPage.tsx
+++ src/pages/WriteReviewPage.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import * as S from "../styles/Home/ViewReviewPageStyle";
-import WriteReview from "../components/WriteReivew";
+import WriteReview from "../components/WriteReview";
 
 const WriteReviewPage = () => {
 	const [menu, setMenu] = useState<string>("메뉴 리뷰")
 
-	return(
+	return (
 		<S.ViewReviewContainer>
 			<S.BtnDiv>
-				<S.ReviewBtn $menu={menu} onClick={()=>{setMenu("메뉴 리뷰")}}>메뉴 리뷰</S.ReviewBtn>
-				<S.WantMenuBtn $menu={menu} onClick={()=>setMenu("먹고 싶은 메뉴")}>먹고 싶은 메뉴</S.WantMenuBtn>
+				<S.ReviewBtn $menu={menu} onClick={() => { setMenu("메뉴 리뷰") }}>메뉴 리뷰</S.ReviewBtn>
+				<S.WantMenuBtn $menu={menu} onClick={() => setMenu("먹고 싶은 메뉴")}>먹고 싶은 메뉴</S.WantMenuBtn>
 			</S.BtnDiv>
-			<WriteReview/>
+			<WriteReview />
 		</S.ViewReviewContainer>
 	);
 };
